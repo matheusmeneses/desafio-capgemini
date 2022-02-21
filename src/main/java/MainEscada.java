c360043a2a874c69251e01f1e2e3c8ffe24b3c14
Matheus <matheus1995@gmail.com>
2022-02-20 23:01:30 -0300

    Adicionando a Main da Senha

--- src/main/java/MainEscada.java
+++ src/main/java/MainEscada.java
@@ -1,6 +1,3 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class MainEscada {
     public static void main(String[] args) {
         Escada escada = new Escada(20);
---
+++ src/main/java/MainSenha.java
@@ -0,0 +1,6 @@
+public class MainSenha {
+    public static void main(String[] args) {
+       Senha senha = new Senha("Maiscula3$");
+        System.out.println(senha.validar());
+    }
+}
